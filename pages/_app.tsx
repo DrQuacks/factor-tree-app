0b8aca7887d19a75bb314665d6cc671325d0683b
Michael Kellar <michael.d.kellar@gmail.com>
2025-07-19 12:35:01 -0700

    added favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,14 +2,25 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { SessionProvider } from 'next-auth/react';
 import type { Session } from 'next-auth';
+import Head from 'next/head';
 
 export default function App({ 
   Component, 
   pageProps: { session, ...pageProps }
 }: AppProps<{ session: Session }>) {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <>
+      <Head>
+        <link rel="icon" type="image/png" href="/images/tree-icon.png" />
+        <link rel="shortcut icon" type="image/png" href="/images/tree-icon.png" />
+        <link rel="apple-touch-icon" href="/images/tree-icon.png" />
+        <link rel="icon" sizes="32x32" type="image/png" href="/images/tree-icon.png" />
+        <link rel="icon" sizes="16x16" type="image/png" href="/images/tree-icon.png" />
+        <meta name="theme-color" content="#4A6FA5" />
+      </Head>
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </>
   );
 }
